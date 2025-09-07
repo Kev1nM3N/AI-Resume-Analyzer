4d4307c4500b844fad28a1f5ef39ce794030c3d2
Kevin <mediaman502@aol.com>
2025-09-07 14:22:50 -0400

    Editing Homepage

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-//17:32
+import { resumes } from "~/constants";
+import ResumeCard from "~/components/ResumeCard";
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "Resumind" },
@@ -8,12 +10,22 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <main>
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+
     <section className="main-section">
         <div className="page-heading">
           <h1>Track Your Applications & Resume Ratings</h1>
           <h2>Review your submissions and check AI-powered feedback.</h2>
         </div>
     </section>
+
+    {resumes.length > 0 && (
+      <div className="resumes-section">
+        {resumes.map((resume) => (
+          <ResumeCard key={resume.id} resume={resume}/>
+        ))}
+      </div>
+    )}
   </main>;
 }
